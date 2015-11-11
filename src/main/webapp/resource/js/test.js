07fa00c41c8ad76b9b8365eb18182e38dbcec4a6
hejiawang <952327407@qq.com>
2015-11-11 13:16:08 +0800

    页面——删

--- src/main/webapp/resource/js/test.js
+++ src/main/webapp/resource/js/test.js
@@ -2,6 +2,7 @@ $(function() {
 
 	$("#gettest").click(function() {
 		var url = "testcontroller/gettest";
+
 		$.ajax({
 			type : 'get',
 			url : url,
@@ -28,6 +29,25 @@ $(function() {
 			error : onerror
 		});
 	});
+
+	$("#deletetest").click(function() {
+		var url = "testcontroller/deltestbyid";
+		$.ajax({
+			type : 'get',
+			url : url,
+			data : {
+				id : $( '#testdiv input[type=checkbox][name=cbDetail]:checked') .val()
+			},
+			dataType : 'json',
+			success : deleteSuccess,
+			error : onerror
+		});
+	});
+
+	$("#updatetest").click(function() {
+		var data = $( '#testdiv input[type=checkbox][name=cbDetail]:checked') .val();
+		alert(data);
+	});
 });
 
 // 处理失败
@@ -52,23 +72,38 @@ function searchSuccess(data) {
 	var html = '<tr><td><input id="cbHead" type="checkbox"/></td><td>ID</td><td>Name</td></tr>';
 	for (var i = 0; i < testData.length; i++) {
 		var testDataInfo = testData[i];
-		html += '<tr><td><input id="cbDetail" type="checkbox" value='
-				+ testDataInfo.id + '/></td><td>' + testDataInfo.id
+		html += '<tr><td><input id="cbDetail" name="cbDetail" type="checkbox" value="'
+				+ testDataInfo.id
+				+ '"/></td><td>'
+				+ testDataInfo.id
 				+ '</td><td>' + testDataInfo.name + '</td></tr>';
 	}
 	$("#testdiv").html(html);
 }
 
 function addSuccess(data) {
-	
+
 	var testid = data;
-	//alert("添加成功");
 	var url = "testcontroller/gettest";
 	$.ajax({
 		type : 'get',
 		url : url,
 		data : {
 			id : testid
+		},
+		dataType : 'json',
+		success : searchSuccess,
+		error : onerror
+	});
+}
+
+function deleteSuccess(data) {
+	var url = "testcontroller/gettest";
+	$.ajax({
+		type : 'get',
+		url : url,
+		data : {
+			id : ""
 		},
 		dataType : 'json',
 		success : searchSuccess,
